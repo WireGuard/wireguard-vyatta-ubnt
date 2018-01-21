521e340a745d65c3d4da1018f8b0db777a0d4cf1
Nils Andreas Svee <me@lochnair.net>
2018-01-21 18:41:35 +0100

    Add script for getting the latest binaries from the build server

---
+++ update_binaries.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+declare -A BOARDS
+BOARDS[e50]=mipsel
+BOARDS[e100]=mips
+BOARDS[e200]=mips
+BOARDS[e300]=mips
+BOARDS[e1000]=mips
+
+for board in "${!BOARDS[@]}"
+do
+	arch="${BOARDS[$board]}"
+
+	echo "Downloading the wg util for $board..."
+	curl -L -o "$board/usr/bin/wg" "https://build.lochnair.net/job/ubiquiti/job/wireguard/lastSuccessfulBuild/artifact/wg-$arch"
+	echo "Downloading the kernel module for $board..."
+	modpath=$(find "$board" -name wireguard.ko)
+	curl -L -o "$modpath" "https://build.lochnair.net/job/ubiquiti/job/wireguard/lastSuccessfulBuild/artifact/wireguard-$board.ko"
+	echo "Done."
+done
